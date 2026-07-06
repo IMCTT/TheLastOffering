9e3cb746a8cbe0d0590902655be7522071fe5719
IMCTT <tomas.tournon@estudiante.imagecampus.edu.ar>
2026-07-06 20:27:41 -0300

    Parcial entregado

---
+++ Source/TheLastOffering/Spells/WaterSpell.cpp
@@ -0,0 +1,2 @@
+﻿#include "C:\Users\Tomi\Desktop\Image\TheLastOffering\Intermediate\Build\Win64\x64\TheLastOfferingEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+#include "WaterSpell.h"
